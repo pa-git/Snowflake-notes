7dd04ebe323268770a873e6912088b214fae5db0
pa-git <pa-git@users.noreply.github.com>
2022-10-25 19:29:41 -0400

    Create Working with semi-structured data.sql

---
+++ Working with semi-structured data.sql
@@ -0,0 +1,7 @@
++-----------------------------------------------------+
+| Working with semi-structured data                   |
+|-----------------------------------------------------|
+| 1- Create table with VARIANT column                 |
+| 2- Load JSON data sample                            |
+| 3- Query table with VARIANT column                  |
++-----------------------------------------------------+
