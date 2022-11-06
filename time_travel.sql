82a9da4b00ef1a4f23f33b693701b65be152517b
pa-git <pa-git@users.noreply.github.com>
2022-11-06 08:17:28 -0500

    Update time_travel.sql

--- time_travel.sql
+++ time_travel.sql
@@ -135,4 +135,40 @@ SELECT * FROM TITANIC
 +-------------+----------+--------+-----------------------------------------------------+--------+-----+-------+-------+------------------+----------+-------+----------+
 5 Row(s) produced. Time Elapsed: 0.267s
 
+-- Accidently drop the table
+DROP TABLE TITANIC;
++-------------------------------+
+| status                        |
+|-------------------------------|
+| TITANIC successfully dropped. |
++-------------------------------+
+1 Row(s) produced. Time Elapsed: 0.496s
+
+-- The table is gone
+SELECT * FROM TITANIC LIMIT 5;
+002003 (42S02): SQL compilation error:
+Object 'TITANIC' does not exist or not authorized.
+
+-- Un-drop the table
+UNDROP TABLE TITANIC;
++--------------------------------------+
+| status                               |
+|--------------------------------------|
+| Table TITANIC successfully restored. |
++--------------------------------------+
+1 Row(s) produced. Time Elapsed: 0.229s
+
+-- The table is back to normal
+SELECT * FROM TITANIC LIMIT 5;
++-------------+----------+--------+-----------------------------------------------------+--------+-----+-------+-------+------------------+----------+-------+----------+
+| PASSENGERID | SURVIVED | PCLASS | NAME                                                | SEX    | AGE | SIBSP | PARCH | TICKET           |     FARE | CABIN | EMBARKED |
+|-------------+----------+--------+-----------------------------------------------------+--------+-----+-------+-------+------------------+----------+-------+----------|
+|           0 |        0 |      3 | Braund, Mr. Owen Harris                             | male   |  22 |     1 |     0 | A/5 21171        |  7.25000 | NULL  | S        |
+|           0 |        1 |      1 | Cumings, Mrs. John Bradley (Florence Briggs Thayer) | female |  38 |     1 |     0 | PC 17599         | 71.28330 | C85   | C        |
+|           0 |        1 |      3 | Heikkinen, Miss. Laina                              | female |  26 |     0 |     0 | STON/O2. 3101282 |  7.92500 | NULL  | S        |
+|           0 |        1 |      1 | Futrelle, Mrs. Jacques Heath (Lily May Peel)        | female |  35 |     1 |     0 | 113803           | 53.10000 | C123  | S        |
+|           0 |        0 |      3 | Allen, Mr. William Henry                            | male   |  35 |     0 |     0 | 373450           |  8.05000 | NULL  | S        |
++-------------+----------+--------+-----------------------------------------------------+--------+-----+-------+-------+------------------+----------+-------+----------+
+5 Row(s) produced. Time Elapsed: 0.132s
+
 
