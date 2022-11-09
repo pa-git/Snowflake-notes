cef8bbf90a38fda0cc3ac2df50a054d1f94f7088
pa-git <pa-git@users.noreply.github.com>
2022-11-09 09:02:05 -0500

    Update security.sql

--- security.sql
+++ security.sql
@@ -1,7 +1,20 @@
--- Dynamic Masking
+--- Network
+------------------------------------------
+-- Network policy
+
+--- Authorization
+------------------------------------------
+-- Role based Access Control (RBAC)
+-- Default roles: 
+
+-- Discretionary Access Control (DAC)
+
+-- Dynamic Data Masking (Column level)
 
 -- Row level security
 
 -- Secure views
 
 -- Secure UDFs
+
+-- Audit logs
