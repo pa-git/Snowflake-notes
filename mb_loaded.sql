0277270960ecbed51650c42b2c5cc5b5f93da560
pa-git <pa-git@users.noreply.github.com>
2025-02-18 16:45:16 -0500

    Update mb_loaded.sql

--- mb_loaded.sql
+++ mb_loaded.sql
@@ -5,3 +5,16 @@ FROM SNOWFLAKE.ACCOUNT_USAGE.COPY_HISTORY
 WHERE load_time >= DATEADD(day, -30, CURRENT_TIMESTAMP())
 GROUP BY load_date
 ORDER BY load_date;
+
+
+SELECT 
+  DATE_TRUNC('day', start_time) AS load_date,
+  SUM(file_size) / (1024*1024) AS mb_loaded
+FROM TABLE(
+  information_schema.copy_history(
+    table_name => 'YOUR_TABLE_NAME',
+    start_time => DATEADD(day, -30, CURRENT_TIMESTAMP())
+  )
+)
+GROUP BY load_date
+ORDER BY load_date;
