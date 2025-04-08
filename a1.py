75c64acc8d61e6e760256b7b35d4adf4f8afe312
pa-git <pa-git@users.noreply.github.com>
2025-04-08 11:22:15 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,13 +1,13 @@
-class GovernanceAndScopeModel(BaseModel):
-    service_level_agreements: List[ServiceLevelAgreement]
-    reporting_and_governance: ReportingAndGovernance
-    engagement_scope: EngagementScope
+import json
 
+with open("overview.json") as f1, open("services_roles.json") as f2, open("governance_scope.json") as f3, open("contextual.json") as f4:
+    overview = json.load(f1)
+    services_roles = json.load(f2)
+    governance_scope = json.load(f3)
+    contextual = json.load(f4)
 
+final_contract_json = {**overview, **services_roles, **governance_scope, **contextual}
 
-class ContextualModel(BaseModel):
-    divisions: List[Division]
-    initiatives: List[Initiative]
-    projects: List[Project]
-    deliverables_and_invoices: List[DeliverableAndInvoice]
-    parties: List[Party]
+# Optionally save it
+with open("full_contract.json", "w") as fout:
+    json.dump(final_contract_json, fout, indent=2)
