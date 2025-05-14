2550d613f3ecbd8a007d6e920faf039b1bc3f6ef
pa-git <pa-git@users.noreply.github.com>
2025-05-14 19:44:21 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,4 +1,5 @@
-def parse_optional_date(date_str):
-    if not date_str:
-        return None
-    return datetime.strptime(date_str, "%d-%b-%Y").date()
+#!/bin/bash
+neo4j stop
+timestamp=$(date +%Y%m%d_%H%M%S)
+cp -r /var/lib/neo4j/data/databases/neo4j /backups/neo4j_backup_$timestamp
+neo4j start
