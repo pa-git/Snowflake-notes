1e1144ef119d81846d8ff8652df876f0e3547ef5
pa-git <pa-git@users.noreply.github.com>
2025-05-16 09:16:38 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,1 +1,8 @@
-To support your answers, you have access to a Neo4j knowledge graph defined by the following schema.
+<SCHEMA>
+  <NODES>
+    ...
+  </NODES>
+  <RELATIONSHIPS>
+    ...
+  </RELATIONSHIPS>
+</SCHEMA>
