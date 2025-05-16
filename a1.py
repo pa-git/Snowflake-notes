57dd220d2d9ab8064eb94ca7c263e2c4c19f1b49
pa-git <pa-git@users.noreply.github.com>
2025-05-15 20:47:15 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,4 +1,4 @@
 def run_cypher_query(query: str):
     results, meta = db.cypher_query(query)
-    headers = [col[0] for col in meta]
+    headers = [col[0] if col[0] else f"col{i}" for i, col in enumerate(meta)]
     return [dict(zip(headers, row + [None] * (len(headers) - len(row)))) for row in results]
