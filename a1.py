3ea3955aeede75486e7df51909549c7f90664e55
pa-git <pa-git@users.noreply.github.com>
2025-05-16 11:13:09 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,3 +1,5 @@
-MATCH (c:Contract)-[:IS_FOR_DIVISION]->(d:CanonicalDivision {name: 'Cyber Data Risk & Resilience'})
-WITH replace(c.total_fee, ",", "") AS fee
-RETURN SUM(toFloat(fee)) AS total_spending
+MATCH (r:Role)-[:IS_CANONICAL_ROLE]->(cr:CanonicalRole)
+MATCH (c:Contract)-[:INCLUDES_ROLE]->(r)
+MATCH (c)-[:IS_FOR_DIVISION]->(d:CanonicalDivision)
+WHERE cr.name CONTAINS 'Software'
+RETURN cr.name, d.name, COUNT(r)
