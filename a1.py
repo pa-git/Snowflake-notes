cf5591e98228754554bc81d04879464f4ebb7ad6
pa-git <pa-git@users.noreply.github.com>
2025-06-18 19:35:23 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,5 +1,1 @@
-# List all .txt files in the directory
-files = [f for f in os.listdir("your_directory") if f.endswith(".txt")]
-
-# Sort numerically based on the number in the filename
-sorted_files = sorted(files, key=lambda x: int(x.split('.')[0]))
+sorted_files = sorted(files, key=lambda x: int(os.path.basename(x).split('.')[0]))
