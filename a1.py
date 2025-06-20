eba8d8ae58f401f50f63246560be15a6c2e238bf
pa-git <pa-git@users.noreply.github.com>
2025-06-19 20:42:22 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,7 +1,2 @@
-def has_file(subfolder_name, base_folder, file_name):
-    """
-    Given a subfolder name, base folder, and file name,
-    return True if the file exists in that subfolder, else False.
-    """
-    full_path = os.path.join(base_folder, subfolder_name, file_name)
-    return os.path.isfile(full_path)
+for i, s in enumerate(data.get("slas", {}).get("service_slas", []), start=1):
+    print(f"  - SLA #{i}")
